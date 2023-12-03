201570dcf8739cf4e13649ddda3691faaa33d2ef
kliu427 <kliu427@uwo.ca>
2023-12-02 21:56:20 -0500

    test

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,16 +3,11 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { SigninComponent } from './signin/signin.component';
 import { CreateaccountComponent } from './createaccount/createaccount.component';
 import { HeaderComponent } from './header/header.component';
-import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent,
-        children: [
-            {path: 'home', component: HomePageComponent},
-            {path: 'signin', component: SigninComponent},
-            {path: 'createaccount', component: CreateaccountComponent}
-        ]
-    }
+    {path: 'home', component: HomePageComponent},
+    {path: 'signin', component: SigninComponent},
+    {path: 'createaccount', component: CreateaccountComponent}
+
+
 ];
